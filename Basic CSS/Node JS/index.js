b47c7ef3d3ca2b9ada65fefaa80ce203a67e37e0
navneet-singh2907 <navnitgujjar@gmail.com>
2025-12-11 20:01:33 -0800

    Initial commit of the basic Node.js console log script

---
+++ Basic CSS/Node JS/index.js
@@ -0,0 +1,6 @@
+console.log("Hello, World! This is a Node.js application.")
+console.log("Hello,.")
+console.log("Hello ji how are you")
+let a = 5;
+let b = 10;
+console.log("The sum of a and b is: " + (a + b));
